a07eab76bbf51ed39670e9e682c5c4c1f752eeee
Grégory Troupenat <gregory.troupenat@gmail.com>
2020-10-06 21:10:53 +0200

    1.2. Les formulaires en PHP - Sécurisation

--- form.php
+++ form.php
@@ -5,37 +5,38 @@
     <title>Simuler une prise de contact - Challenge</title>
 </head>
 <body>
-    <form  action="thanks.php"  method="post">
+    <form action="thanks.php" method="post">
         <div>
-            <label  for="nom">Nom :</label>
-            <input  type="text"  id="nom"  name="user_lastname">
+            <label for="nom">Nom :</label>
+            <input type="text" id="nom" name="user_lastname" required>
         </div>
         <div>
-            <label  for="prenom">Prénom :</label>
-            <input  type="text"  id="prenom"  name="user_firstname">
+            <label for="prenom">Prénom :</label>
+            <input type="text" id="prenom" name="user_firstname" required>
         </div>
         <div>
-            <label  for="courriel">Courriel :</label>
-            <input  type="email"  id="courriel"  name="user_email">
+            <label for="courriel">Courriel :</label>
+            <input type="email" id="courriel" name="user_email" required>
         </div>
         <div>
-            <label  for="phone_number">Tél. :</label>
-            <input  type="tel"  id="phone_number"  name="user_phone">
+            <label for="phone_number">Tél. :</label>
+            <input type="tel" id="phone_number" name="user_phone" required>
         </div>
         <div>
-            <label  for="sujet">Sujet :</label>
-            <select id="sujet" name="user_subject">
+            <label for="sujet">Sujet :</label>
+            <select id="sujet" name="user_subject" required>
+                <option value="">Sélectionnez un sujet</option>
                 <option value="sujet1">Sujet 1</option>
                 <option value="sujet2">Sujet 2</option>
                 <option value="sujet3">Sujet 3</option>
             </select>
         </div>
         <div>
-            <label  for="message">Message :</label>
-            <textarea  id="message"  name="user_message"></textarea>
+            <label for="message">Message :</label>
+            <textarea id="message" name="user_message" required></textarea>
         </div>
-        <div  class="button">
-            <button  type="submit">Envoyer votre message</button>
+        <div class="button">
+            <button type="submit" name="button-form">Envoyer votre message</button>
         </div>
     </form>
 </body>
--- thanks.php
+++ thanks.php
@@ -1,3 +1,32 @@
+<?php
+if (!empty($_POST) && isset($_POST['button-form'])) {
+
+    $errors = [];
+
+    $lastname = trim($_POST['user_lastname']);
+    $firstname = trim($_POST['user_firstname']);
+    $email = trim($_POST['user_email']);
+    $phone = trim($_POST['user_phone']);
+    $subject = trim($_POST['user_subject']);
+    $message = trim($_POST['user_message']);
+
+    if (empty($lastname))
+        $errors['user_lastname'] = 'Nom requis';
+    if (empty($firstname))
+        $errors['user_firstname'] = 'Prénom requis';
+    if (empty($email))
+        $errors['user_email'] = 'Courriel requis';
+    if (!empty($email) && !filter_var($email, FILTER_VALIDATE_EMAIL))
+        $errors['user_email'] = 'Courriel non valide';
+    if (empty($phone))
+        $errors['user_phone'] = 'Numéro de téléphone requis';
+    if (empty($subject))
+        $errors['user_subject'] = 'Sujet requis';
+    if (empty($message))
+        $errors['user_message'] = 'Message requis';
+}
+?>
+
 <!DOCTYPE html>
 <html lang="fr">
 <head>
@@ -5,8 +34,28 @@
     <title>Simuler une prise de contact - Challenge</title>
 </head>
 <body>
-<p>Merci <?=$_POST['user_firstname']?> <?=$_POST['user_lastname']?> de nous avoir contacté à propos de “<?=$_POST['user_subject']?>”.</p>
-<p>Un de nos conseiller vous contactera soit à l’adresse <?=$_POST['user_email']?> ou par téléphone au <?=$_POST['user_phone']?> dans les plus brefs délais pour traiter votre demande :</p>
-<p><?=$_POST['user_message']?></p>
+<?php if (isset($errors['user_lastname'])): ?>
+    <p style="color:red;"><?= $errors['user_lastname'] ?></p>
+<?php endif; ?>
+<?php if (isset($errors['user_firstname'])): ?>
+    <p style="color:red;"><?= $errors['user_firstname'] ?></p>
+<?php endif; ?>
+<?php if (isset($errors['user_email'])): ?>
+    <p style="color:red;"><?= $errors['user_email'] ?></p>
+<?php endif; ?>
+<?php if (isset($errors['user_phone'])): ?>
+    <p style="color:red;"><?= $errors['user_phone'] ?></p>
+<?php endif; ?>
+<?php if (isset($errors['user_subject'])): ?>
+    <p style="color:red;"><?= $errors['user_subject'] ?></p>
+<?php endif; ?>
+<?php if (isset($errors['user_message'])): ?>
+    <p style="color:red;"><?= $errors['user_message'] ?></p>
+<?php endif; ?>
+
+<p>Merci <?= $_POST['user_firstname']; ?> <?= $_POST['user_lastname']; ?> de nous avoir contacté à propos de “<?= $_POST['user_subject']; ?>”.</p>
+<p>Un de nos conseiller vous contactera soit à l’adresse <?= $_POST['user_email']; ?> ou par téléphone au <?= $_POST['user_phone']; ?> dans les plus brefs délais pour traiter votre demande :</p>
+<p><?= $_POST['user_message']; ?></p>
+
 </body>
 </html>
